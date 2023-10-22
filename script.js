ebc0e83105c93a8cbd54138046be68f89b6faf4e
ThakareSakshi <sakshithakare121@gmail.com>
2023-10-22 22:07:54 +0530

    minor changes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let input = document.querySelector("input");
 let search_btn = document.querySelector(".search");
-
+let show_all_cards = document.querySelector(".show_all_cards")
 let phone_container = document.querySelector(".phones");
 search_btn.addEventListener("click", () => {
     logPhones()
@@ -10,11 +10,12 @@ search_btn.addEventListener("click", () => {
 default_mobiles()
 
 async function default_mobiles() {
+    // let show_all_cards=document.querySelector(".show_all_cards")
 
     let response = await fetch("https://openapi.programming-hero.com/api/phones?search=Iphone");
     const phones = await response.json();
     console.log(phones);
-    phones.data.forEach(element => {
+    phones.data.forEach((element, index) => {
         let card = document.createElement("div");
         card.setAttribute("class", "card");
         card.innerHTML = `<img src="${element.image}" alt="">
@@ -22,7 +23,11 @@ async function default_mobiles() {
     <p>There are many variations of passages of available, but the majority have suffered</p>
     <button class="show_detail_btn" onClick=showDetails("${element.slug}")>show details</button>`
 
-        phone_container.appendChild(card);
+        if (index >= 9) {
+            show_all_cards.appendChild(card);
+        } else {
+            phone_container.appendChild(card);
+        }
 
     });
 
@@ -33,26 +38,32 @@ async function showDetails(slug) {
     console.log(detailsInfo)
 }
 
-
+let show_container=document.createElement("div")
 async function logPhones() {
     let response = await fetch("https://openapi.programming-hero.com/api/phones?search=" + input.value);
     const phones = await response.json();
+   
     let new_container = document.createElement("div");
-    phones.data.forEach(element => {
+    phones.data.forEach((element,index) => {
         let card = document.createElement("div");
         card.setAttribute("class", "card");
         card.innerHTML = `<img src="${element.image}" alt="">
     <h2>${element.phone_name}</h2>
     <p>There are many variations of passages of available, but the majority have suffered</p>
     <button class="show_detail_btn" onClick=showDetails("${element.slug}")>show details</button>`
-
-        new_container.appendChild(card);
+        if (index >= 9) {
+            show_all_cards.appendChild(card);
+        } else {
+           show_container.appendChild(card);
+        }
+       
 
 
 
 
 
     });
+     new_container.innerHTML=show_container.innerHTML;
     phone_container.innerHTML = new_container.innerHTML
 
 
@@ -68,7 +79,7 @@ async function showDetails(slug) {
 function addDetailsToContainer(data) {
     console.log(data);
     let dialog = document.querySelector("dialog");
-    let dialog_container=document.querySelector(".dialog_container")
+    let dialog_container = document.querySelector(".dialog_container")
     dialog.innerHTML = `<img src="${data.image}" alt="">
    <h2>${data.name}</h2>
    <p>Brand :${data.brand}<p>
@@ -81,14 +92,27 @@ function addDetailsToContainer(data) {
    <br>
      <button id="close">close</button>`
 
-   dialog_container.style.display="block";
- let close_btn=document.querySelector("#close")
-close_btn.addEventListener("click",()=>{
-    console.log("closed")
-    dialog_container.style.display="none";
-})
+    dialog_container.style.display = "block";
+    let close_btn = document.querySelector("#close")
+    close_btn.addEventListener("click", () => {
+        console.log("closed")
+        dialog_container.style.display = "none";
+    })
 
 }
+
+
+let show_All_btn = document.querySelector("#show_btn");
+show_All_btn.addEventListener("click", () => {
+    if (show_All_btn.innerText == "SHOW ALL") {
+        show_All_btn.innerText = "SHOW LESS"
+        show_all_cards.style.display="flex";
+    }
+    else {
+        show_All_btn.innerText = "SHOW ALL"
+        show_all_cards.style.display="none";
+    }
+})
 
 
 
